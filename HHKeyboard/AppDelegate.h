b52656ca67f4f37eedd959d60bf91878f5520cdf
henry <1325049637@qq.com>
2021-06-12 00:11:40 +0800

    Initial Commit

---
+++ HHKeyboard/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HHKeyboard
+//
+//  Created by Henry on 2021/6/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
